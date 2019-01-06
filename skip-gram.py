5a2ae57eafbdd40f1dd268ffe934a5007ad7e085
Rookro <kaito.acer.akr@gmail.com>
2019-01-07 05:31:06 +0900

    ファイル変更

--- skip-gram.py
+++ skip-gram.py
@@ -14,20 +14,9 @@ sentences = word2vec.Text8Corpus(os.path.join(cur_dir, "corpus.juman"))
 model = word2vec.Word2Vec(sentences, size = 300, min_count = 3, window = 15, iter = 8, sg = 1, workers = 8)
 
 #model.int_sims(replace = True)
-model.save("skipgram_gensim_min_3")
+model.save("skipgram_gensim")
 
-model = word2vec.Word2Vec.load("skipgram_gensim_min_3")
+model = word2vec.Word2Vec.load("skipgram_gensim")
 
-print(">>> model.most_similar(positive=['王','女性'], negative=['男性']))")
-print(model.most_similar(positive=['王','女性'], negative=['男性']))
-print(">>> model.most_similar(positive=['王様','女性'], negative=['男性'])")
-print(model.most_similar(positive=['王様','女性'], negative=['男性']))
-print('>>> model.most_similar("アニメ")')
-print(model.most_similar("アニメ"))
-print('>>> model.most_similar("テレビ")')
-print(model.most_similar("テレビ"))
-print('>>> model.most_similar("マンガ")')
-print(model.most_similar("マンガ"))
-print('>>> model.most_similar("ジャンプ")')
-print(model.most_similar("ジャンプ"))
+
 
